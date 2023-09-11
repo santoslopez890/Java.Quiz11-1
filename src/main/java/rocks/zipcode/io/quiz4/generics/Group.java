87cb245ca4560901f1ff3aa5bd994cead4d9fca0
santos <santoslopez890@gmail.com>
2023-09-11 13:51:24 -0400

    Did some things moved on blah blah blah

--- src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
@@ -1,37 +1,49 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author leon on 18/12/2018.
  */
 public class Group<_> {
+    List<_> newList;
     public Group() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        newList=new ArrayList<>();
+
     }
 
     public Integer count() {
-        return null;
+        return newList.size();
     }
 
     public void insert(_ value) {
+        newList.add(value);
     }
 
     public Boolean has(_ value) {
-        return null;
+        return newList.contains(value);
     }
 
     public _ fetch(int indexOfValue) {
-        return null;
+        return newList.get(indexOfValue-1);
     }
 
     public void delete(_ value) {
+        newList.remove(value);
     }
 
     public void clear() {
+        newList.clear();
     }
 
     public Iterator<_> iterator() {
-        return null;
+        return newList.iterator();
+    }
+
+    @Override
+    public String toString() {
+        return newList.toString();
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/generics/MyStack.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/MyStack.java
@@ -1,25 +1,57 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class MyStack<SomeType> {
+public class MyStack<SomeType> implements Iterable<SomeType>{
+    private ArrayList<SomeType> myStack;
     public MyStack() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+myStack=new ArrayList<>();
     }
 
     public Boolean isEmpty() {
-        return null;
+        return myStack.isEmpty();
     }
 
     public void push(SomeType i) {
+        myStack.add(i);
     }
 
-    public SomeType peek() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+    public SomeType peek() throws EmptyStackException{
+        if(myStack.isEmpty()){
+            EmptyStackException EmptyStackException = new EmptyStackException();
+            throw EmptyStackException;
+        }
+        return myStack.get(myStack.size()-1);
+
     }
 
-    public SomeType pop() {
+    public SomeType pop() throws EmptyStackException {
+        if(myStack.isEmpty()){
+            EmptyStackException EmptyStackException = new EmptyStackException();
+            throw EmptyStackException;
+        }
+        return myStack.remove(myStack.size() - 1);
+    }
+
+    @Override
+    public Iterator<SomeType> iterator() {
         return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super SomeType> action) {
+        Iterable.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<SomeType> spliterator() {
+        return Iterable.super.spliterator();
     }
 }
